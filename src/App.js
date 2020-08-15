361addaef43f8f4256f5bf837a79198d2b719490
jorgevz <jorgev2132@gmail.com>
2020-08-15 18:20:05 -0400

    dog pic alt implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './component/Navbar';
+import Form from './component/Form'
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
-     <Paw_Form/>
+     <Form/>
     </div>
   );
 }
