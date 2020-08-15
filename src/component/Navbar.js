e946ba56fa1014a10b693ba1dfec9efa4e722a74
jorgevz <jorgev2132@gmail.com>
2020-08-15 18:01:25 -0400

    p tags

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -6,8 +6,8 @@ function Navbar (){
         <section>
        
         <nav>
-        <li>Home</li>
-        <li>Form</li>
+        <p>Home</p>
+        <p>Form</p>
         <h2>Pawtastic</h2>
         <img src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq" height='300px' width='300px'/>
         </nav>
