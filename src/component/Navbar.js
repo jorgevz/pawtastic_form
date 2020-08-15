ae8a0da472f2d0be1328706413c70b4f89e0cf2d
jorgevz <jorgev2132@gmail.com>
2020-08-15 18:00:27 -0400

    extensive edits

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -9,7 +9,7 @@ function Navbar (){
         <li>Home</li>
         <li>Form</li>
         <h2>Pawtastic</h2>
-        <img src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq" height='100px' width='100px'/>
+        <img src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq" height='300px' width='300px'/>
         </nav>
     
         </section>
