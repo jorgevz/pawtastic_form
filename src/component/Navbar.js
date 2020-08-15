78e6293be530230eab12050a3168aa855a41d1a3
jorgevz <jorgev2132@gmail.com>
2020-08-15 18:38:29 -0400

    border radius taken down

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -5,7 +5,7 @@ function Navbar (){
         <div className = "title">
        
         <nav>
-        <p>Home</p>
+        <h3>Home</h3>
         <h2>Pawtastic</h2>
         <img src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq" height='300px' width='300px' alt='dogpic'/>
         </nav>
