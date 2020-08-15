624de9064032be84caba14e970f66488f34c7ba6
jorgevz <jorgev2132@gmail.com>
2020-08-15 18:20:20 -0400

    alt

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,7 +7,7 @@ function Navbar (){
         <nav>
         <p>Home</p>
         <h2>Pawtastic</h2>
-        <img src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq" height='300px' width='300px'/>
+        <img src="https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq" height='300px' width='300px' alt='dogpic'/>
         </nav>
     </div>
     );
